f855ded09bb22aff8e1fd2ea0d583bc4edb124bf
Iván Garrido (Adalab professor) <ivan@adalab.es>
2024-02-24 15:22:19 +0100

    Implement Question component with JSON data.

--- src/components/pages/FormPage.jsx
+++ src/components/pages/FormPage.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
 import Forms from "../forms";
 function FormPage ( {
+  questions,
+  updateAnswer,
+  data,
   updateAvatarAuthor,
 })
 {
+  
 return (
     <section>
       
-      <Forms updateAvatarAuthor={updateAvatarAuthor}/>
+      <Forms updateAvatarAuthor={updateAvatarAuthor} updateAnswer={updateAnswer} data={data} questions={questions} />
       <Link className="btn" to="/">Volver al inicio{""} </Link>
       </section>
 )};
