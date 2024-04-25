5e11c7bba1f8b01e5eea710d9d34de2e1a07c299
Elena Valero <elenavaleroesco@gmail.com>
2024-04-25 20:17:34 +0200

    "fix scss profile page"

--- frontend-react/src/components/pages/ProfilePage.jsx
+++ frontend-react/src/components/pages/ProfilePage.jsx
@@ -5,8 +5,8 @@ import { useEffect, useState } from "react";
 
 function ProfilePage() {
   const navigate = useNavigate();
-  const [profileData, setProfileData] = useState({})
-  const [error, setError] = useState()
+  const [profileData, setProfileData] = useState({});
+  const [error, setError] = useState();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -14,26 +14,31 @@ function ProfilePage() {
 
     fetch("http://localhost:8080/profile", {
       headers: { Authorization: token },
-    }).then(async (response) => {
-      const data = await response.json();
-      console.log(data)
-      setProfileData(data)
-      console.log(profileData)
-      setError(null)
-    }).catch((error)=>{
-        setError('No se pudo cargar los datos del usuario')
-    });
+    })
+      .then(async (response) => {
+        const data = await response.json();
+        console.log(data);
+        setProfileData(data);
+        console.log(profileData);
+        setError(null);
+      })
+      .catch((error) => {
+        setError("No se pudo cargar los datos del usuario");
+      });
   }, []);
 
   return (
-    // TODO PONER EL ERROR DE QUE NO SE HAN PODIDO CARGAR LOS DATOS
-    <section className="section">
-      <h2 className="profile_title">Mi perfil</h2>
-      <h3 className="name">{profileData.name}</h3>
-      <h4 className="city">{profileData.address} </h4>
-      <h4 className="city">{new Date(profileData.birthdate).getFullYear()} </h4>
-      <div className="image">
-        <img className="image_img" src={profileData?.image} alt="mi imagen" />
+    <section>
+      <div className="section">
+        <h2 className="profile_title">Mi perfil</h2>
+        <div className="image">
+          <img className="image_img" src={profileData?.image} alt="mi imagen" />
+        </div>
+        <h3 className="name">{profileData.name}</h3>
+        <h4 className="city">{profileData.address} </h4>
+        <h4 className="city">
+          {new Date(profileData.birthdate).getFullYear()}{" "}
+        </h4>
       </div>
 
       <Link className="btn" to="/forms">
